5a4ac16e49ea2fbce38c107f5640c2b5796b3e96
maipov <phammaiphuong9103@gmail.com>
2025-03-25 05:28:46 +0700

    Add files via upload

---
+++ BankAccount (1).cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountManager
+{
+    public class BankAccount
+    {
+        public string Owner { get; private set; }
+        public decimal Balance { get; private set; }
+        public bool IsActive { get; private set; }
+        public bool IsClosed { get; private set; }
+        public BankAccount(string owner)
+        {
+            Owner = owner;
+            Balance = 0;
+            IsActive = false;
+            IsClosed = true;
+        }
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0) throw new ArgumentException("Amount must be positive.");
+            Balance += amount;
+
+            if (!IsActive)
+            {
+                IsActive = true;
+                Console.WriteLine("Account reactivated");
+            }
+        }
+        public bool Withdraw(decimal amount, bool identityVerified)
+        {
+            if (IsClosed) throw new InvalidOperationException("Account is closed.");
+            if (!IsActive) throw new InvalidOperationException("Account is inactive.");
+            if (!identityVerified) throw new UnauthorizedAccessException("Identity verification required.");
+            if (amount > Balance) throw new InvalidOperationException("Insufficient fund.");
+
+            Balance -= amount;
+            return true;
+        }
+
+        public void CloseAccount()
+        { 
+            IsClosed = true;
+            IsActive = false;
+        }
+        public void DeactivateAccount() 
+        {
+            if (!IsClosed) IsActive = false;
+        }
+    }
+    
+}
